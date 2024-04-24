c62fb216a6b916c9b2d55462debb483466c2af30
Mateus Ribeiro <mateus_ribeiro21@hotmail.com>
2024-04-23 23:02:07 -0300

    feat: implements findByID on prisma question repository

--- src/infra/database/prisma/repositories/prisma-questions-repository.ts
+++ src/infra/database/prisma/repositories/prisma-questions-repository.ts
@@ -1,16 +1,30 @@
 import { PaginationParams } from '@/core/repositories/pagination-params'
 import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
 import { Question } from '@/domain/forum/enterprise/entities/question'
+import { PrismaQuestionMapper } from '@/infra/database/prisma/mappers/prisma-question-mapper'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
 import { Injectable } from '@nestjs/common'
 
 @Injectable()
 export class PrismaQuestionsRepository implements QuestionsRepository {
+  constructor(private prisma: PrismaService) {}
+
   findBySlug(slug: string): Promise<Question | null> {
     throw new Error('Method not implemented.')
   }
 
-  findById(id: string): Promise<Question | null> {
-    throw new Error('Method not implemented.')
+  async findById(id: string): Promise<Question | null> {
+    const question = await this.prisma.question.findUnique({
+      where: {
+        id,
+      },
+    })
+
+    if (!question) {
+      return null
+    }
+
+    return PrismaQuestionMapper.toDomain(question)
   }
 
   findManyRecent(params: PaginationParams): Promise<Question[]> {
